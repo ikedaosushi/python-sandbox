e8574cdde6c6fbcd689ad8a1a453e4ed2478796e
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2019-11-25 17:07:25 +0800

    Add

--- az-personalize/create.sh
+++ az-personalize/create.sh
@@ -3,10 +3,10 @@ STACK_NAME=amazon-personalize-example-stack
 REGION=ap-northeast-1
 TEMPLATE_FILE=file://cloudformation.yml
 
-# aws cloudformation create-stack \
-#   --stack-name $STACK_NAME \
-#   --region $REGION \
-#   --template-body $TEMPLATE_FILE
+aws cloudformation create-stack \
+  --stack-name $STACK_NAME \
+  --region $REGION \
+  --template-body $TEMPLATE_FILE
 
 aws cloudformation update-stack \
   --stack-name $STACK_NAME \
